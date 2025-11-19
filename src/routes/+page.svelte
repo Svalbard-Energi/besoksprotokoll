5ac825b190db4d9f0c9c2017361a56a8727ac020
Sneisen <jorgen.sneisen@svalbard-energi.no>
2025-11-19 11:10:04 +0100

    try sveltekit gh pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Test Besøksprotokoll</h1>
+<embed src="test.pdf" width="100%" />
+<iframe
+  title="Besøksprotokoll"
+  width="640px"
+  height="480px"
+  src="https://forms.office.com/Pages/ResponsePage.aspx?id=kAfOvZoOMk6H46LhSGvhwbXCj2A_N6ROpHGpez2MNg1UNVRWSDNXUUlLNEVDTVlCWE4wUElGRDg4WSQlQCN0PWcu&embed=true"
+  frameborder="0"
+  marginwidth="0"
+  marginheight="0"
+  style="border: none; max-width: 100%; max-height: 100vh"
+  allowfullscreen
+  webkitallowfullscreen
+  mozallowfullscreen
+  msallowfullscreen
+>
+</iframe>
