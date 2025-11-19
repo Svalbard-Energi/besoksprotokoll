3d7e12364656b22dc81d86ea434079ae94c3d7a0
Sneisen <jorgen.sneisen@svalbard-energi.no>
2025-11-19 13:55:40 +0100

    Add pdf viewer

---
+++ docs/scripts/immutable/nodes/2.DWwofQ2b.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/D6p-2RWZ.js";export{m as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import PDFViewer from '$lib/PDFViewer.svelte';
 </script>
 
 <div class="flex h-full flex-col">
 	<h1 class="text-lg font-bold">1. Les HMS introduksjonen</h1>
-	<iframe title="HMS intro" class="h-full w-full" src="test.pdf#toolbar=0"></iframe>
+	<PDFViewer></PDFViewer>
 	<a
 		class="m-1 inline-flex items-center justify-center self-start rounded-lg bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700"
 		href={resolve('/register')}>Jeg bekrefter å ha lest og forstått informasjonen over</a
