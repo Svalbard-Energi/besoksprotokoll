4be179ac5a2a4134d52cf526e89e548865e89f11
Sneisen <jorgen.sneisen@svalbard-energi.no>
2025-11-19 12:28:33 +0100

    page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,14 @@
-<h1>Test Besøksprotokoll</h1>
+<script lang="ts">
+	import { resolve } from "$app/paths";
+</script>
+
+<h1>1. Les HMS introduksjonen</h1>
 <embed src="test.pdf" width="100%" />
-<iframe
-  title="Besøksprotokoll"
-  width="640px"
-  height="480px"
-  src="https://forms.office.com/Pages/ResponsePage.aspx?id=kAfOvZoOMk6H46LhSGvhwbXCj2A_N6ROpHGpez2MNg1UNVRWSDNXUUlLNEVDTVlCWE4wUElGRDg4WSQlQCN0PWcu&embed=true"
-  frameborder="0"
-  marginwidth="0"
-  marginheight="0"
-  style="border: none; max-width: 100%; max-height: 100vh"
-  allowfullscreen
-  webkitallowfullscreen
-  mozallowfullscreen
-  msallowfullscreen
->
-</iframe>
+<a href={resolve("/register")}>Jeg bekrefter å ha lest og forstått informasjonen over</a>
+
+<style>
+  embed {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,20 @@
+<h1>2. Registrer deg i protokollen</h1>
+<iframe
+  title="Besøksprotokoll"
+  width="640px"
+  height="480px"
+  src="https://forms.office.com/Pages/ResponsePage.aspx?id=kAfOvZoOMk6H46LhSGvhwbXCj2A_N6ROpHGpez2MNg1UNVRWSDNXUUlLNEVDTVlCWE4wUElGRDg4WSQlQCN0PWcu&embed=true"
+  frameborder="0"
+  marginwidth="0"
+  marginheight="0"
+  allowfullscreen
+>
+</iframe>
+
+<style>
+    iframe {
+        border: none;
+        max-width: 100%;
+        height: 100%;
+    }
+</style>
